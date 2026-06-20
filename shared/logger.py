dea4d30a32689ffd9d84da462e6bed56f210a614
Milica-Drljaca <drljacam9@gmail.com>
2026-06-20 18:34:18 +0200

    Koristi najnoviju verziju prometheus instrumentatora bez fiksiranih konfliktnih zavisnosti

--- shared/logger.py
+++ shared/logger.py
@@ -4,6 +4,6 @@ from prometheus_fastapi_instrumentator import Instrumentator
 def setup_metrics(app, service_name: str):
     Instrumentator(
         should_group_status_codes=True,
-        should_ignore_untemplated=False,
+        should_ignore_untemplated=True,
         excluded_handlers=["/metrics"],
     ).instrument(app).expose(app)
